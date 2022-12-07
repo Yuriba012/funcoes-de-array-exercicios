c03c3cea95eec9cfe7478016a61f987de914f3a2
unknown <yuriba012@gmail.com>
2022-12-07 15:42:35 -0300

    resolucao exercicios de funcoes de array

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,3 +4,23 @@ const objeto = {
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+
+const caixaAlta = objeto =>{
+    const novoObjeto = {}
+    for (frase in objeto)
+        novoObjeto[frase] = objeto[frase].toUpperCase()
+    return novoObjeto
+}
+
+const textoCorrido = objeto =>{
+    let texto = ''
+    for (key in objeto){
+        texto += `${objeto[key]}\n`
+    }
+    return texto
+}
+
+const imprimir = (objeto, callback) => console.log(callback(objeto))
+
+imprimir(objeto, caixaAlta)
+imprimir(objeto, textoCorrido)
---
+++ exercicio2/script.js
@@ -0,0 +1,8 @@
+const numeros = [5,48,62,54,36,14,52,6]
+
+const triplos = numeros.map(numero => numero * 3)
+
+const metades = numeros.map(numero => numero / 2)
+
+console.log(triplos)
+console.log(metades)
